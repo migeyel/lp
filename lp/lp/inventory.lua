07f6101fcc49d1d4eafa1b5eebec3ea63090463d
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2023-02-20 16:33:15 -0300

    Fix inventory initialization race condition

--- lp/lp/inventory.lua
+++ lp/lp/inventory.lua
@@ -2,21 +2,30 @@ local abstractInvLib = require "abstractInvLib"
 local mutex = require "lp.mutex"
 local log = require "lp.log"
 
+local invStartupMutex = mutex()
 local inv = nil
 
 ---@return AbstractInventory
 local function get()
-    if inv then return inv end
+    local guard = invStartupMutex.lock()
+    if inv then
+        guard.unlock()
+        return inv
+    end
+
     log:info("Starting inventory")
 
     local storage = { peripheral.find("inventory") }
     for i, v in pairs(storage) do storage[i] = peripheral.getName(v) end
-    inv = abstractInvLib(storage)
+    local linv = abstractInvLib(storage)
     inv.refreshStorage()
     inv.defrag()
 
     log:info("Inventory ready")
-    return inv
+    inv = linv
+
+    guard.unlock()
+    return linv
 end
 
 return {
