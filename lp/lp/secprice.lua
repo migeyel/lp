2b57bab954c595239cfbe497ea040f4310896a04
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2024-02-03 14:41:18 -0300

    Remove security krist allocation

--- lp/lp/secprice.lua
+++ lp/lp/secprice.lua
@@ -1,34 +1,14 @@
-local wallet = require "lp.wallet"
 local pools = require "lp.pools"
 local inventory = require "lp.inventory"
 local threads = require "lp.threads"
 
 local SECURITY_TAG = "LP Security"
-local SEC_KST_SUM_PART = 0.1
-local SEC_KST_MAX_REALLOC_PART = 0.05
 local SEC_ITEMS_TARGET = 512
 local SEC_ITEMS_MAX_REALLOC_PART = 8
 
 ---@return Pool
 local function getSecPool()
     return assert(pools.getByTag(SECURITY_TAG), "failed to find sec pool")
-end
-
----@param commit boolean
-local function reallocKrist(commit)
-    local pool = getSecPool()
-    local targetKst = pools.totalKrist() * SEC_KST_SUM_PART
-    local diff = targetKst - pool.allocatedKrist
-    if math.abs(diff) < 0.5 then return end
-    local upperBound = SEC_KST_MAX_REALLOC_PART * pool.allocatedKrist
-    local lowerBound = -SEC_KST_MAX_REALLOC_PART * pool.allocatedKrist
-    diff = math.min(upperBound, diff)
-    diff = math.max(lowerBound, diff)
-    diff = math.min(diff, wallet.getSecFund())
-    wallet.reallocateSec(-diff, false)
-    pool:reallocKst(diff, false)
-    if commit then pools.state:commitMany(wallet.state) end
-    pools.priceChangeEvent.queue(pool:id())
 end
 
 local function reallocItems()
@@ -59,9 +39,10 @@ end
 
 threads.register(function()
     while true do
-        sleep(math.random() * 2)
-        reallocKrist(true)
-        reallocItems()
+        sleep(1)
+        if math.random(1, 3600) == 1 then
+            reallocItems()
+        end
     end
 end)
 
